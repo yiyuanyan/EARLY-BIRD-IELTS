47d349ff101d215bbc13355842a7cc6f8cff8251
何建新 <yiyuanhanly@126.com>
2016-06-25 17:47:59 +0800

    22334455

--- EARLY-BIRD-IELTS/PostDetails/PostInfoViewController.m
+++ EARLY-BIRD-IELTS/PostDetails/PostInfoViewController.m
@@ -28,7 +28,7 @@
     self.navigationItem.leftBarButtonItem = leftItem;
     self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 64, kWidth, [UIScreen mainScreen].bounds.size.height)];
     self.scrollView.showsVerticalScrollIndicator = NO;
-    self.scrollView.backgroundColor = [UIColor redColor];
+    self.scrollView.backgroundColor = [UIColor colorWithRed:200/255.0 green:200/255.0 blue:200/255.0 alpha:1];
     
     NSString *url = [NSString stringWithFormat:@"http://testapp.benniaoyasi.com/api.php?appid=1&m=api&devtype=android&uuid=Anv7Hq60Kbtx73UOYk-8j_Z_Mw8Q3pCP8avAGO7Z22HR&version=2.0&mobile=15810910848&uid=182676&c=Nbbs&a=infoBbs&id=%@",self.postId];
     [self getNetWorkingJSON:url requestType:@"contentView"];
@@ -84,46 +84,48 @@
 //创建帖子详情内容页面
 -(void)createContentView:(NSDictionary *)info{
     NSLog(@"%@",info);
+    //主贴内容View
+    UIView *ContentView = [[UIView alloc] init];
     //头像
-    UIImageView *userImageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, -64, 40, 40)];
+    UIImageView *userImageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 0, 40, 40)];
     [userImageView sd_setImageWithURL:[NSURL URLWithString:info[@"uimage"]]];
     userImageView.layer.masksToBounds = YES;
     userImageView.layer.cornerRadius = userImageView.bounds.size.width * 0.5;
-    [self.scrollView addSubview:userImageView];
+    [ContentView addSubview:userImageView];
     //名称
-    UILabel *userName = [[UILabel alloc] initWithFrame:CGRectMake(60, -64, [self boundingRectWithSize:CGSizeMake(kWidth, 0) string:info[@"uname"]].width+10, 40)];
+    UILabel *userName = [[UILabel alloc] initWithFrame:CGRectMake(60, 0, [self boundingRectWithSize:CGSizeMake(kWidth, 0) string:info[@"uname"]].width+10, 40)];
     userName.font = kFont;
     userName.text = info[@"uname"];
-    [self.scrollView addSubview:userName];
+    [ContentView addSubview:userName];
     //发帖时间
-    UILabel *time = [[UILabel alloc] initWithFrame:CGRectMake(kWidth-[self boundingRectWithSize:CGSizeMake(kWidth, 0) string:info[@"create_time_geshi"]].width-20, -64, [self boundingRectWithSize:CGSizeMake(kWidth, 0) string:info[@"create_time_geshi"]].width+10, 40)];
+    UILabel *time = [[UILabel alloc] initWithFrame:CGRectMake(kWidth-[self boundingRectWithSize:CGSizeMake(kWidth, 0) string:info[@"create_time_geshi"]].width-20, 0, [self boundingRectWithSize:CGSizeMake(kWidth, 0) string:info[@"create_time_geshi"]].width+10, 40)];
     time.font = kFont;
     time.text = info[@"create_time_geshi"];
     time.textColor = [UIColor colorWithRed:200/255.0 green:200/255.0 blue:200/255.0 alpha:1];
-    [self.scrollView addSubview:time];
+    [ContentView addSubview:time];
     //考试时间
     UIImageView *timeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(18, userName.frame.origin.y+userName.frame.size.height+20, 24, 24)];
     timeImageView.image = [UIImage imageNamed:@"ic_access_time"];
-    [self.scrollView addSubview:timeImageView];
+    [ContentView addSubview:timeImageView];
     float kaoshiLabelW = [self boundingRectWithSize:CGSizeMake(kWidth, 0) string:info[@"exam_time"]].width;
     UILabel *kaoshiLabel = [[UILabel alloc] initWithFrame:CGRectMake(47, timeImageView.frame.origin.y, kaoshiLabelW+70, 24)];
     kaoshiLabel.font = kFont;
     kaoshiLabel.text = [NSString stringWithFormat:@"考试时间：%@",info[@"exam_time"]];
-    [self.scrollView addSubview:kaoshiLabel];
+    [ContentView addSubview:kaoshiLabel];
     //考试地点
     UIImageView *didianImageView = [[UIImageView alloc] initWithFrame:CGRectMake(timeImageView.frame.origin.x, timeImageView.frame.origin.y+timeImageView.frame.size.height+10, 24, 24)];
     didianImageView.image = [UIImage imageNamed:@"icon_position"];
-    [self.scrollView addSubview:didianImageView];
+    [ContentView addSubview:didianImageView];
     float didianLabelW = [self boundingRectWithSize:CGSizeMake(kWidth, 0) string:info[@"school_name"]].width+70;
     UILabel *didianLabel = [[UILabel alloc] initWithFrame:CGRectMake(kaoshiLabel.frame.origin.x, didianImageView.frame.origin.y, didianLabelW, 24)];
     didianLabel.font = kFont;
     didianLabel.text = [NSString stringWithFormat:@"考试地点：%@",info[@"school_name"]];
-    [self.scrollView addSubview:didianLabel];
+    [ContentView addSubview:didianLabel];
     //Part1栏目
     UILabel *part1Label = [[UILabel alloc] initWithFrame:CGRectMake(didianLabel.frame.origin.x,didianLabel.frame.origin.y+didianLabel.frame.size.height+10,  40, 35)];
     part1Label.font = kFont;
     part1Label.text = @"Part1";
-    [self.scrollView addSubview:part1Label];
+    [ContentView addSubview:part1Label];
     float part1BtnW = [self boundingRectWithSize:CGSizeMake(kWidth, 0) string:info[@"p1name"]].width;
     if(part1BtnW > (kWidth-(part1Label.frame.origin.x+part1Label.frame.size.width+10))){
         part1BtnW = (kWidth-(part1Label.frame.origin.x+part1Label.frame.size.width+10));
@@ -137,13 +139,13 @@
     [part1Btn.layer setBorderWidth:1.0];
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     [part1Btn.layer setBorderColor:CGColorCreate(colorSpace, (CGFloat[]){240/255.0,240/255.0,240/255.0,240/255.0})];
-    [self.scrollView addSubview:part1Btn];
+    [ContentView addSubview:part1Btn];
     //Part2栏目
     
     UILabel *part2Label = [[UILabel alloc] initWithFrame:CGRectMake(part1Label.frame.origin.x, part1Label.frame.origin.y+part1Label.frame.size.height+10, 40, 35)];
     part2Label.font = kFont;
     part2Label.text = @"Part2";
-    [self.scrollView addSubview:part2Label];
+    [ContentView addSubview:part2Label];
     float part2BtnW = [self boundingRectWithSize:CGSizeMake(kWidth, 0) string:info[@"p2name"]].width;
     if(part2BtnW > (kWidth-(part2Label.frame.origin.x+part2Label.frame.size.width+10))){
         part2BtnW = (kWidth-(part2Label.frame.origin.x+part2Label.frame.size.width+10));
@@ -156,30 +158,56 @@
     [part2Btn.layer setCornerRadius:18.0];
     [part2Btn.layer setBorderWidth:1.0];
     [part2Btn.layer setBorderColor:CGColorCreate(colorSpace, (CGFloat[]){240/255.0,240/255.0,240/255.0,240/255.0})];
-    [self.scrollView addSubview:part2Btn];
+    [ContentView addSubview:part2Btn];
     //帖子内容
     float contentLabelH = [self boundingRectWithSize:CGSizeMake(kWidth, 0) string:info[@"content"]].height;
     UILabel *contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(5, part2Btn.frame.origin.y+part2Btn.frame.size.height+10, kWidth-10, contentLabelH)];
     contentLabel.numberOfLines = 0;
     contentLabel.font = kFont;
     contentLabel.text = info[@"content"];
-    [self.scrollView addSubview:contentLabel];
+    [ContentView addSubview:contentLabel];
     //图片
     int imgY = contentLabel.frame.origin.y+contentLabel.frame.size.height+10;
     //float imgViewH = 0.0;
     //UIView *imgView = [[UIView alloc] init];
     if([info[@"images"] count]>=1){
     for(int i=0; i<[info[@"images"] count];i++){
-        
+        NSLog(@"%d",i);
         UIImageView *imageView = [[UIImageView alloc] init];
         [imageView sd_setImageWithURL:[NSURL URLWithString:info[@"images"][i][@"middle_image"]]];
-        [imageView setFrame:CGRectMake(5, imgY, imageView.image.size.width, imageView.image.size.height)];
-        [self.scrollView addSubview:imageView];
-        imgY += imageView.image.size.height+5;
-        i++;
+        [imageView setFrame:CGRectMake(5, imgY, imageView.image.size.width/3*2, imageView.image.size.height/3*2)];
+        [ContentView addSubview:imageView];
+        imgY += (imageView.image.size.height/3*2)+5;
+
     }
     }
-    self.scrollView.contentSize = CGSizeMake(0, imgY);
+    ContentView.backgroundColor = [UIColor whiteColor];
+    [ContentView setFrame:CGRectMake(0, -64, kWidth, imgY)];
+    [self.scrollView addSubview:ContentView];
+    //点赞人数View
+    UIView *praiseView = [[UIView alloc] initWithFrame:CGRectMake(0, ContentView.frame.origin.y+ContentView.frame.size.height+10, kWidth, 90)];
+    praiseView.backgroundColor = [UIColor whiteColor];
+    //点赞人数Label
+    UILabel *praiseLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 90)];
+    praiseLabel.font = kFont;
+    praiseLabel.text = [NSString stringWithFormat:@"%@人赞了该帖",info[@"praise_num"]];
+    praiseLabel.textAlignment = NSTextAlignmentCenter;
+    [praiseView addSubview:praiseLabel];
+    float imgViewX = praiseLabel.frame.origin.x+praiseLabel.frame.size.width;
+    for(int i=0;i<[info[@"praise_info"] count];i++){
+        UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(imgViewX, 30, 30, 30)];
+        [imgView sd_setImageWithURL:[NSURL URLWithString:info[@"praise_info"][i][@"uimage"]]];
+        imgViewX += 40;
+        [praiseView addSubview:imgView];
+    }
+    [self.scrollView addSubview:praiseView];
+    
+    
+    
+    
+    
+    self.scrollView.contentSize = CGSizeMake(0, praiseView.frame.origin.y+praiseView.frame.size.height+74);
+    
     
 }
 @end
